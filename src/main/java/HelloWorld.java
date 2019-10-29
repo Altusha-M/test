309e4359dffbfe8a3f81adcd40b3ff570d400c17
Altynov Mikhail <altynov.m@gmail.com>
2019-10-29 12:40:13 +0300

    29.10 Homework task for lesson 1. Added sout at HelloWorld and new comment at pom.xml

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, teacher!");
+        System.out.println("This string I added at my laptop");
     }
 }
