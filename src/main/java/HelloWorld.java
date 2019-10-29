8734e441f0d71eef8c5fef83077b46a878954282
Altusha-M <55461879+Altusha-M@users.noreply.github.com>
2019-10-29 12:50:26 +0300

    Add new sout line in HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, teacher!");
-        System.out.println("This string I added at my laptop");
+        System.out.println("This string I have added at my laptop(and a little rewrite at GitHub");
+        System.out.println("This string I have added at GitHub");
     }
 }
